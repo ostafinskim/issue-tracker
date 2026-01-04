2f26bc008ff5c561da9e446e05a70ce6492d90d7
Miro Ostafinski <ostafinskim@gmail.com>
2026-01-04 14:56:40 +0000

    auth middleware

---
+++ middleware.ts
@@ -0,0 +1,21 @@
+import { headers } from "next/headers";
+import { NextResponse, type NextRequest } from "next/server";
+
+
+export const middleware = async (request: NextRequest) => {
+	if (request.nextUrl.pathname.startsWith('/api')) {
+		const authHeader = (await headers()).get('Authorization')
+
+		if (!authHeader) {
+			return NextResponse.json(
+				{ success: false, message: 'Authorization header is required' },
+				{ status: 401 }
+			)
+		}
+	}
+	return NextResponse.next()
+}
+
+export const config = {
+	matcher: '/api/:path*'
+}
